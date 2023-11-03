dc18a7b42d49a039cec83d5b266a570423eab277
haydarde <48902837+haydarde@users.noreply.github.com>
2023-11-03 14:41:54 +1100

    Add files via upload

--- createdata_2D.R
+++ createdata_2D.R
@@ -1,11 +1,13 @@
 #' JAGS model specification for spatiotemporal epidemiological modelling
 #' @author Dinah Lope/ Haydar Demirhan
-#' @version 1.1
-#' @created 07/06/2022
+#' @version 2.0
+#' @created 12/10/2023
 #' @license GPLv3
 #' 
-#' 
 
+options(scipen = 999)
+graphics.off()
+rm(list=ls())
 library(rjags)
 library(runjags)
 library(coda)
@@ -13,46 +15,59 @@ library(nimble)
 library(magrittr)
 library(tidyverse)
 library(reshape2)
+load.module("GeoJAGS")
 
-source("0 settings.R")
 source("DBDA2E-utilities.R")
-
 count <- 0
 tmp1 <- list()
 tmp2 <- list()
 tmp3 <- list()
-
 set.seed(2021)
+# Create a random adjacency matrix
+num_area <- 10
+num_neighbours <- 5
+adj <- matrix(0, nrow = num_area, ncol = num_area)
+upperIndicies <- which(upper.tri(adj, diag = FALSE), arr.ind = TRUE)
+indices <- (as.matrix(upperIndicies[sample(1:nrow(upperIndicies), num_neighbours , replace = F),]))
+adj[indices] <- 1
+adj[(indices[,c(2,1)])] <- 1
+
 for (num_records in c(100, 500, 1000, 2000, 4000, 8000, 10000)){
   for (numSavedSteps in c(100, 500, 1000, 2000, 4000, 5000, 10000)){
+    cat("number of records = ",num_records,"\n")
+    cat("number of saved steps = ",numSavedSteps,"\n" )
     
     count <- count + 1
-    num_area <- 1:10
-    num_time <- 1:(num_records/(num_area %>% length()))
     
-    s <- num_area %>% length() 
+    areas <- 1:10
+    num_time <- 1:(num_records/(areas %>% length()))
+    
+    s <- areas %>% length()
     t <- num_time %>% length() 
     if((s*t == num_records) %in% TRUE){"ok proceed"}else{"wrong matrix count"}
-    
+
     zdata <- rpois(num_records, 10)
-    covariate1 <- runif(num_records)
+    covariate1 <-  runif(num_records)
     
     region_index <- NULL
     for(i in 1:10){
-      region_index[i] <- num_area[i]
+      region_index[i] <- areas[i]
     }
+    
     if((region_index %>% length() == s) %in% TRUE){"ok proceed"}else{"wrong index count"}
     
     data_all <- tibble(cov = covariate1, 
                        flu = zdata, 
                        datetime = data.frame(tmp = rep(num_time, s)) %>% arrange(tmp) %>% pull(tmp),
-                       lga = rep(num_area, t)) %>% as.data.frame()
+                       lga = rep(areas, t)) %>% as.data.frame()
     
     flu_constants = list(x_var = acast(data_all, lga~datetime, value.var="cov"),
                          # index = region_index,
                          z =  acast(data_all, lga~datetime, value.var="flu"),
                          space = s,
-                         time = t)
+                         time = t,
+                         mu = array(0,num_records),
+                         adj = adj)
                          # n = num_records)
     
     # Specify the model
@@ -60,19 +75,20 @@ for (num_records in c(100, 500, 1000, 2000, 4000, 8000, 10000)){
       model {
         for(s in 1:space){
           for(t in 1:time){
-            lambda[s,t] <- exp(a[1] + x_var[s,t]*a[2] + theta[s])
+            lambda[s,t] <- exp(a[1] + x_var[s,t]*a[2] + theta[s] + phi[s])
             z[s,t] ~ dpois(lambda[s,t])
           }
         }
         for(j in 1:space){
-          theta[j] ~ dnorm(0, 1/10)
+          theta[j] ~ dnorm(0, 0.10)
         }
         for(i in 1:2){
-          a[i] ~ dnorm(0, 1/10)
+          a[i] ~ dnorm(0, 0.10)
         }
+        phi[1:space] ~ dmnorm(mu[1:space], precMatrixCAR(adj[1:space, 1:space], 0.999, 10))
       }
-    " # close quote for modelString
-    writeLines(modelString , con="TEMPmodel.txt")
+    " 
+    writeLines(modelString , con="TEMPmodel2D.txt")
     
     # Generate chains
     parameters <- c("a[1]", "a[2]")
@@ -83,41 +99,32 @@ for (num_records in c(100, 500, 1000, 2000, 4000, 8000, 10000)){
       }
     }
     for (i in 1:s){
-      parameters <- c(parameters , 
-                      paste0("theta[",i,"]"))
+      parameters <- c(parameters, 
+                      paste0("theta[",i,"]"), paste0("phi[",i,"]"))
     }
-
+    # parameters
+    
     options(scipen = 999)
     adaptSteps = 500
     burnInSteps = 100
     nChains = 1
     thinSteps = 1
-    # numSavedSteps = 1000
     nIter = ceiling((numSavedSteps*thinSteps)/nChains)
-    # nIter
-    
-    set.seed(2021)
+
     startTime = proc.time()
-    jmodel <- jags.model("TEMPmodel.txt",
+    jmodel <- jags.model("TEMPmodel2D.txt",
                          data=flu_constants,
                          n.chains=nChains,
-                         #inits=flu_inits,
+                         # inits=inits,
                          n.adapt=adaptSteps)
+
     update(jmodel, n.iter = burnInSteps)
     samples <- coda.samples(jmodel, variable.names=parameters, n.iter=nIter, thin = thinSteps)
     stopTime = proc.time()
     tmp_b = stopTime - startTime
-    
-    tmp_a <- paste(numSavedSteps, "numSavedSteps", nIter, "nIter", num_records, "num_records")
-    # tmp_b <- system.time(update(jmodel, n.iter = burnInSteps))
-    # samples <- coda.samples(jmodel, variable.names=parameters, n.iter=nIter, thin = thinSteps)
-    
-    tmp1[[count]] <- list(tmp_a, tmp_b[1], tmp_b[2], tmp_b[3], samples)
-    tmp2[[count]] <- list(tmp_a, tmp_b[1], tmp_b[2], tmp_b[3], samples)
-    tmp3[[count]] <- list(tmp_a, tmp_b[1], tmp_b[2], tmp_b[3])
+    tmp_a <- paste(numSavedSteps, "; numSavedSteps; ", nIter, "; nIter ;", num_records, "; num_records ;")
+    tmp3 <- rbind(tmp3,data.frame(tmp_a, tmp_b[1], tmp_b[2], tmp_b[3]))
   }
 }
 
-save(tmp1, tmp2, file = "data_all_nested.RData")
-
-
+write.csv(tmp3, file = "data_100Iter_2D_HD_tmp3_GEOJags.csv")
